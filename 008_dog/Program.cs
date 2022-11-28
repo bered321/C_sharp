fec46a0de89bcc332ad3f2de125a34a7beda2d35
be_red <bered321@inbox.ru>
2022-11-28 13:37:30 +0300

    wrote code for solving the problem about the dog

--- 008_dog/Program.cs
+++ 008_dog/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int count = 0, distance = 10000,
+    firstFriendSpeed = 1, secondFriendSpeed = 2,
+    dogSpeed = 5, time = 0, friend = 2;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+Console.WriteLine("Собака пробежит " + count + " раз");
+
