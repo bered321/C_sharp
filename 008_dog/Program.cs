a61c856becd8ca9856aff40a85db46aa896a9e03
be_red <bered321@inbox.ru>
2022-11-29 10:27:14 +0300

    add Seminars

--- 008_dog/Program.cs
+++ 008_dog/Program.cs
@@ -1,6 +1,6 @@
 ﻿int count = 0, distance = 10000,
     firstFriendSpeed = 1, secondFriendSpeed = 2,
-    dogSpeed = 5, time = 0, friend = 2;
+    dogSpeed = 5, time, friend = 2;
 
 while (distance > 10)
 {
@@ -14,8 +14,8 @@ while (distance > 10)
         time = distance / (secondFriendSpeed + dogSpeed);
         friend = 1;
     }
-    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
-    count++;
+distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+count++;
 }
 Console.WriteLine("Собака пробежит " + count + " раз");
 
