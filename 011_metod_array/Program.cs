1c7359fa5266c0f70a0ea4005f71a01412261280
be_red <bered321@inbox.ru>
2022-11-30 20:49:36 +0300

    add new folder array library

--- 011_metod_array/Program.cs
+++ 011_metod_array/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 1, 22, 23, 37, 49, 67, 32, 49 };
+int n = array.Length;
+int find = 49;
+int index=0;
+
+while(index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
