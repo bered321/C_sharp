5bd6a3b1c72e8600cd7638064fadc92c62ec2d73
be_red <bered321@inbox.ru>
2022-12-07 19:07:27 +0300

    add new folder methods

--- 011_metod_array/Program.cs
+++ 011_metod_array/Program.cs
@@ -1,11 +1,11 @@
 ﻿int[] array = { 1, 22, 23, 37, 49, 67, 32, 49 };
 int n = array.Length;
 int find = 49;
-int index=0;
+int index = 0;
 
-while(index<n)
+while (index < n)
 {
-    if(array[index]==find)
+    if (array[index] == find)
     {
         Console.WriteLine(index);
         break;
